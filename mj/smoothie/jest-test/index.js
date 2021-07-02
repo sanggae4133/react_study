6a082864f925aca9cc5ca84011a5914e010d91b6
omjinLTS <omjin7n@naver.com>
2021-07-03 03:02:27 +0900

    0703

---
+++ mj/smoothie/jest-test/index.js
@@ -0,0 +1,30 @@
+const sum = (a, b) => {
+    return a + b;
+};
+
+const person = (name, age) => {
+    return {
+        name,
+        age,
+    };
+};
+
+const toggle = (a) => {
+    return !a;
+};
+
+const range = (start, end) => {
+    let result = [];
+    for (let i = start; i <= end; i++) {
+        result.push(i);
+    };
+    return result;
+};
+
+//module exports
+module.exports = {
+    sum,
+    person,
+    toggle,
+    range,
+};
